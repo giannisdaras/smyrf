abe882ce87a89c08a3303542706b5296c3a23f58
Giannis Daras <daras.giannhs@gmail.com>
2020-04-27 22:42:13 +0300

    Add removed code for ILSVRC dataset

--- examples/celeba/datasets.py
+++ examples/celeba/datasets.py
@@ -7,6 +7,7 @@ import os
 from tqdm import tqdm
 import torch.utils.data as data
 import torch_xla.core.xla_model as xm
+import h5py as h5
 
 
 class CelebAHQ(data.Dataset):
@@ -37,12 +38,64 @@ class CelebAHQ(data.Dataset):
         return self.num_images
 
 
+'''
+    ILSVRC_HDF5: A dataset to support I/O from an HDF5 to avoid
+        having to load individual images all the time.
+    Source code: https://github.com/ajbrock/BigGAN-PyTorch/blob/master/datasets.py
+'''
+class ILSVRC_HDF5(data.Dataset):
+  def __init__(self, root, transform=None, target_transform=None,
+               load_in_mem=False, train=True,
+               download=False, validate_seed=0,
+               val_split=0, **kwargs): # last four are dummies
 
-if __name__ == '__main__':
-    transform = transforms.Compose([transforms.ToTensor(), lambda x: x.cuda()])
-    dataset = CelebAHQ('CelebAMask-HQ/CelebA-HQ-img', transform, True)
-    loader = torch.utils.data.DataLoader(dataset=dataset,
-                                         batch_size=1,
-                                         num_workers=6,
-                                         shuffle=True,
-                                         drop_last=True)
+    self.root = root
+    self.num_imgs = len(h5.File(root, 'r')['labels'])
+
+    # self.transform = transform
+    self.target_transform = target_transform
+
+    # Set the transform here
+    self.transform = transform
+
+    # load the entire dataset into memory?
+    self.load_in_mem = load_in_mem
+
+    # If loading into memory, do so now
+    if self.load_in_mem:
+      print('Loading %s into memory...' % root)
+      with h5.File(root,'r') as f:
+        self.data = f['imgs'][:]
+        self.labels = f['labels'][:]
+
+  def __getitem__(self, index):
+    """
+    Args:
+        index (int): Index
+    Returns:
+        tuple: (image, target) where target is class_index of the target class.
+    """
+    # If loaded the entire dataset in RAM, get image from memory
+    if self.load_in_mem:
+      img = self.data[index]
+      target = self.labels[index]
+
+    # Else load it from disk
+    else:
+      with h5.File(self.root,'r') as f:
+        img = f['imgs'][index]
+        target = f['labels'][index]
+
+
+    # if self.transform is not None:
+        # img = self.transform(img)
+    # Apply my own transform
+    img = ((torch.from_numpy(img).float() / 255) - 0.5) * 2
+
+    if self.target_transform is not None:
+      target = self.target_transform(target)
+
+    return img, int(target)
+
+  def __len__(self):
+      return self.num_imgs
