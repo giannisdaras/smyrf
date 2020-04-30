61c69c6067476dc737c86de6d988d2f22a7c182c
Giannis Daras <daras.giannhs@gmail.com>
2020-04-30 18:03:43 +0000

    Remove python list for XLA compatibility

--- smyrf/torch/utils.py
+++ smyrf/torch/utils.py
@@ -337,12 +337,12 @@ class XBOXPLUS(AsymmetricTransform):
 
     def K(self, x):
         ext = torch.sqrt(self.MQ**2 + self.MK**2 - self.k_norms**2)
-        zero = torch.tensor([0.0], device=x.device).repeat(x.shape[:-1], 1).unsqueeze(-1)
+        zero = torch.tensor(0.0, device=x.device).repeat(x.shape[:-1], 1).unsqueeze(-1)
         return torch.cat((x, ext, zero), -1)
 
     def Q(self, x):
         ext = torch.sqrt(self.MQ**2 + self.MK**2 - self.q_norms**2)
-        zero = torch.tensor([0.0], device=x.device).repeat(x.shape[:-1], 1).unsqueeze(-1)
+        zero = torch.tensor(0.0, device=x.device).repeat(x.shape[:-1], 1).unsqueeze(-1)
         return torch.cat((x, zero, ext), -1)
 
 
