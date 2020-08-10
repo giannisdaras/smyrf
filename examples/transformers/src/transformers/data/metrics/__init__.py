2700fd41baab1bddad540e0325a31f7cfe393e29
Giannis Daras <daras.giannhs@gmail.com>
2020-08-10 12:30:33 +0300

    Add hyperpartisan to downstream

--- examples/transformers/src/transformers/data/metrics/__init__.py
+++ examples/transformers/src/transformers/data/metrics/__init__.py
@@ -78,6 +78,8 @@ if _has_sklearn:
             return {"acc": simple_accuracy(preds, labels)}
         elif task_name == "hans":
             return {"acc": simple_accuracy(preds, labels)}
+        elif task_name == "hyperpartisan":
+            return acc_and_f1(preds, labels)
         else:
             raise KeyError(task_name)
 
